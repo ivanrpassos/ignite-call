da25b59ea3b65ceb4d115decf2819dc571f15caf
Ivan Passos <ivanpassosfilho@gmail.com>
2023-06-07 00:43:21 -0300

    Aula 7 - Utilizando React Hook Form

--- src/pages/components/ClaimUsernameForm/index.tsx
+++ src/pages/components/ClaimUsernameForm/index.tsx
@@ -7,10 +7,31 @@ import { ArrowRight } from 'phosphor-react'
 // Styles
 import { Form } from './styles'
 
+// Form validation
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const claimUsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
+
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuário" />
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuário"
+        {...register('username')}
+      />
       <Button size="sm" type="submit">
         Reservar
         <ArrowRight />
