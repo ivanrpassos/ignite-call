b51d6933a427d20e6775fe0a52a68797119ef30d
Ivan Passos <ivanpassosfilho@gmail.com>
2023-06-24 07:04:37 -0300

    Aula 41 - Formulário de confirmação

--- src/pages/components/ScheduleForm/index.tsx
+++ src/pages/components/ScheduleForm/index.tsx
@@ -1,5 +1,6 @@
 // import { CalendarStep } from '../CalendarStep'
-import { ConfirmStep } from '../ConfirmStep'
+
+import ConfirmStep from '../ConfirmStep'
 
 export function ScheduleForm() {
   return <ConfirmStep />
